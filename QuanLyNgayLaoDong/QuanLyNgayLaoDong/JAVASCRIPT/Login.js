8105232f5e675445f2cc862b9c096a97362e3e59
Huynhtienhieu1234 <huynhtienhieubasao@gmail.com>
2025-05-24 22:32:41 +0700

    thêm CSS javascript login

---
+++ QuanLyNgayLaoDong/QuanLyNgayLaoDong/JAVASCRIPT/Login.js
@@ -0,0 +1,13 @@
+﻿function togglePassword() {
+    const passwordField = document.getElementById("passwordField");
+    const eyeIcon = document.getElementById("eyeIcon");
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        eyeIcon.classList.remove("bi-eye-slash");
+        eyeIcon.classList.add("bi-eye");
+    } else {
+        passwordField.type = "password";
+        eyeIcon.classList.remove("bi-eye");
+        eyeIcon.classList.add("bi-eye-slash");
+    }
+}
